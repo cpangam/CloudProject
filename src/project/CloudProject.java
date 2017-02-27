80e902b3893d1225cd80fa7379c26be35ee42eed
rohansumant <r.s.sumant@gmail.com>
2017-02-27 13:52:33 -0800

    Init

---
+++ src/project/CloudProject.java
@@ -0,0 +1,12 @@
+package project;
+
+/**
+ * Created by rohansumant on 2/13/17.
+ */
+
+public class CloudProject {
+    public static void main(String[] args) {
+        MainAlgorithm a = new MainAlgorithm(new int[] {1,3,4,8,12,14,24},11);
+        System.out.println(a.infer());
+    }
+}
